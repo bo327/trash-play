05d007619a78bfbaecf159f31200ecf167ddc0fd
bo327 <80336157+bo327@users.noreply.github.com>
2021-06-13 10:02:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# trash-play
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+
+var gnd, paper, trash;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	gnd = new ground(width/2 , 670, width, 20);
+	paper = new ball(200,450,20);
+	trash = new bin(1200,650);
+
+
+
+   
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+
+  gnd.display();
+  paper.display();
+  trash.display();
+  
+  drawSprites();
+ 
+}
+
+
+function keyPressed()
+{
+	if(keyCode === UP_ARROW)
+	{
+		Matter.Body.applyForce(paper.body ,paper.body.position, { x:25, y:-30});
+	}
+}
+
+
+
